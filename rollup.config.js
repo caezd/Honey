f815f987fee0bb3433ae6400553d52bcca215498
Monomer <caedrikbl@gmail.com>
2025-03-01 08:58:37 -0500

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 /* import commonjs from "@rollup/plugin-commonjs"; */
-=======
-import { uglify } from 'rollup-plugin-uglify';
->>>>>>> 02ddcc05f22dd13d26696fdf6915ea94c69a1aa8
 
 export default {
     input: "src/main.js",
@@ -12,10 +8,5 @@ export default {
         format: "umd",
         name: "Honey",
     },
-<<<<<<< HEAD
     plugins: [nodeResolve({ browser: true })],
-=======
-    external: ['jquery'],
-    /* plugins: [uglify()] */
->>>>>>> 02ddcc05f22dd13d26696fdf6915ea94c69a1aa8
 };
